e10628173c562a03d1afe243200e46f8997fbf44
housirzero <housirzero@gmail.com>
2018-05-19 01:18:50 +0800

    add comment

--- CDemo/CDemo/Array.hpp
+++ CDemo/CDemo/Array.hpp
@@ -12,11 +12,12 @@
 #include <stdio.h>
 #include <assert.h>
 
+template <class T>
 class Array
 {
 private:
     int _size;
-    int * array;
+    T * _array;
     static const int DEFAULT_SIZE = 10;
 public:
     // explicit 防止隐式转换（a == b[i]）情况的出现
@@ -24,27 +25,45 @@ public:
     {
         assert(size > 0);
         _size = size;
-        array = new int[_size];
-        if(NULL == array)
+        _array = new T[_size];
+        if(NULL == _array)
         {
             // throw exception
         }
     }
     
+    Array(const Array& obj);
+    
+    Array(T * array, int size);
+    
     ~Array()
     {
-        delete array;
-        array = NULL;
+        // 这里不必判断_array是否为空，因为delete NULL值也是合法的，底层会判断
+        delete [] _array;
+        _array = NULL;
     }
     
+    // const 在后面，修饰整个函数，不允许此函数对成员变量进行修改
+    // 在类内定义的成员函数会被自动当做内联(inline)函数
     int size() const {return _size;};
-    int max() const;
-    int min() const;
-    int mean() const;
-    int mid() const;
+    T max() const;
+    T min() const;
+    T mean() const;
+    T mid() const;
     
     void sort();
+    void reverse();
     
+    /**
+     Description
+            对象赋值
+     @param obj
+            参数类型为引用，避免对象拷贝，提升效率
+            参数为const，避免对对象进行修改
+     @return 
+            返回类型为对象本身，方便连续赋值：a=b=c
+            返回类型为引用，避免对象拷贝，提升效率
+     */
     Array& operator =(const Array& obj);
     Array& operator ==(const Array& obj);
     Array& operator !=(const Array& obj);
@@ -56,9 +75,13 @@ public:
     Array& operator *(int value);
     Array& operator /(int value);
     
-    int& operator [](int index);
+    T& operator [](int index)
+    {
+        assert(index >= 0 && index < _size);
+        return _array[index];
+    }
     
-    int find_first_of(int target, int start_pos = 0);
+    int find_first_of(T target, int start_pos = 0) const;
 };
 
 #endif /* Array_hpp */
